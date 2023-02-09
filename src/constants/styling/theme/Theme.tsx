641858faed2de70871dffc555d5740e0feb42bb3
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:44:37 -0600

    Theme Object

--- src/constants/styling/theme/Theme.tsx
+++ src/constants/styling/theme/Theme.tsx
@@ -1,5 +1,10 @@
-import ThemeInterface from "../../interfaces/ThemeInterface";
-
-const Theme: ThemeInterface = {};
+const Theme: ThemeInterface = {
+  lightCyanColor: "hsl(193, 38%, 86%)",
+  neonGreenColor: "hsl(150, 100%, 66%)",
+  greyishBlueColor: "hsl(217, 19%, 38%)",
+  darkGreyishBlueColor: "hsl(217, 19%, 24%)",
+  darkBlueColor: "hsl(218, 23%, 16%)",
+  fontSize: "28px",
+};
 
 export default Theme;
