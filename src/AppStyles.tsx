130e7f4ebff9048a1040865a326b90adde288c8c
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:45:08 -0600

    Added Global Theme

--- src/AppStyles.tsx
+++ src/AppStyles.tsx
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+import { Row } from "./constants/styling/layout/Flex";
+
+const AppContainer = styled(Row)`
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default AppContainer;
