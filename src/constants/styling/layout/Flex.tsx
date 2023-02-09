b6c0cfeb23c22689860167fc283506f809b6630e
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:44:22 -0600

    Flex Box Layout

--- src/constants/styling/layout/Flex.tsx
+++ src/constants/styling/layout/Flex.tsx
@@ -1,1 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export { Column, Row };
