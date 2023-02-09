c89c3c3c4507d74729ddc3e469a7e00d4a857f87
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:45:27 -0600

    Type Declaration File

---
+++ src/declaration.d.ts
@@ -0,0 +1,13 @@
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.svg';
+
+interface ThemeInterface {
+    lightCyanColor: "hsl(193, 38%, 86%)",
+    neonGreenColor: "hsl(150, 100%, 66%)",
+    greyishBlueColor: "hsl(217, 19%, 38%)",
+    darkGreyishBlueColor: "hsl(217, 19%, 24%)",
+    darkBlueColor: "hsl(218, 23%, 16%)",
+    fontSize: "28px"
+};
+
