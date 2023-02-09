9191f8508a4f512ebaeabf5df1f6875c96ee08af
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:45:45 -0600

    Added Theme Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { AppGlobalTheme } from "./indexStyles";
+import { ThemeProvider } from "styled-components";
+import Theme from "./constants/styling/theme/Theme";
 const ele = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(ele);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <AppGlobalTheme />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
