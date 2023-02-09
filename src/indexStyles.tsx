1d74ae665d6704bd950783c2c590e220e34ed82e
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:46:19 -0600

    App Global Theme Styles

--- src/indexStyles.tsx
+++ src/indexStyles.tsx
@@ -1,1 +1,27 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const AppGlobalTheme = createGlobalStyle`
+
+    * {
+
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+    }
+
+    body {
+    
+        font-family: Manrope;
+        background: ${({ theme }: { theme: ThemeInterface }) =>
+          theme.darkBlueColor};
+
+    }
+
+    li {
+
+        list-style: none;
+
+    }
+
+`;
