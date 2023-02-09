921fa15a4c390cff8bc31b35e65e27e76b05207b
Kweayon  Clark <Kweayon@gmail.com>
2023-02-08 23:44:53 -0600

    App Component Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import AppContainer from "./AppStyles";
+import AdviceCard from "./components/adviceCard/AdviceCard";
+import Theme from "./constants/styling/theme/Theme";
 
-const App = (props:{}) => {
-	return (
-	<div> App </div>
-)
+const App = (props: {}) => {
+  return (
+    <AppContainer>
+      <AdviceCard />
+    </AppContainer>
+  );
 };
 
 export default App;
