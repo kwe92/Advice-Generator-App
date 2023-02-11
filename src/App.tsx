ecbf39bc405c6be8efbb77b81607bbf39226d15a
Kweayon  Clark <Kweayon@gmail.com>
2023-02-10 23:57:06 -0600

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AppContainer from "./AppStyles";
 import AdviceCard from "./components/adviceCard/AdviceCard";
+import useFetch from "./constants/custom_hooks/useFetch";
 import Theme from "./constants/styling/theme/Theme";
 
 const App = (props: {}) => {
+  const [quote, setQuote] = useFetch();
   return (
     <AppContainer>
       <AdviceCard />
