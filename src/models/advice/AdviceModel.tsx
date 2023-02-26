adb801fbd037f8178205e9fa371e185d18835312
Kweayon  Clark <Kweayon@gmail.com>
2023-02-25 23:46:45 -0600

    Created Advice Model MobX State Tree Model

--- src/models/advice/AdviceModel.tsx
+++ src/models/advice/AdviceModel.tsx
@@ -1,17 +1,66 @@
-import { types } from "mobx-state-tree";
+import axios from "axios";
+import { flow, types } from "mobx-state-tree";
 
 const RandomNumber = (min: number, max: number) => {
   const randNum = Math.round(Math.random() * (max - min) + min);
   return randNum;
 };
 
-const AdviceModel = types.model({
-  id: types.optional(types.identifier, "1"),
-  advice: types.optional(types.string, ""),
-});
+const AdviceModel = types
+  .model({
+    id: types.identifier,
+    advice: types.string,
+  })
+  .actions((self) => ({
+    addAdvice(advice: Advice) {
+      self.id = advice.id;
+      self.advice = advice.advice;
+    },
+  }));
 
-const AdviceStore = types.model({
-  advice: types.map(AdviceModel),
-});
+const AdviceStore = types
+  .model({
+    advice: types.array(AdviceModel),
+    state: types.optional(
+      types.enumeration(["pending", "done", "error"]),
+      "pending"
+    ),
+  })
+  .actions((self) => ({
+    addAdvice(advice: Advice) {
+      self.advice.pop();
+      self.advice.push(advice);
+    },
+    fetchAdvice: flow(function* () {
+      self.state = "pending";
+      console.log("State: ", self.state);
+      const endpoint = "https://api.adviceslip.com/advice";
+      try {
+        const response = yield axios.get(endpoint);
+        const slip = response.data.slip;
+        const adviceObj = { id: String(slip.id), advice: slip.advice };
 
-console.log("Random Number: ", RandomNumber(2, 10));
+        console.log("Advice: ", adviceObj);
+
+        self.advice.pop();
+        self.advice.push(adviceObj);
+        self.state = "done";
+
+        console.log("State: ", self.state);
+      } catch (error) {
+        console.error("Failed to fetch advice!", error);
+        self.state = "error";
+      }
+    }),
+  }));
+
+const store = AdviceStore.create({});
+
+const fetchData = async () => {
+  await store.fetchAdvice();
+  console.log("Store Values: ", store.advice.values().next());
+};
+
+fetchData();
+
+export default AdviceStore;
